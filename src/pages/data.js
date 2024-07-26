ca35c10299dc23986192a623fad597af5b2a39e8
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-07-26 14:30:06 -0300

    Agrego variables de entorno

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,16 +1,9 @@
 export const prerender = false;
 
 export async function GET() {
-  const urlPosnet =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRk6IZqqnmSDcuNEe5ymrviwG259ciagRbszQ-rl1I6wIsDEbwkQs1O67hvQZUNkGIdGcUYskbZyXhi/pub?gid=0&single=true&output=csv";
+  const { URL_POSNET, URL_BANCOS, URL_CUOTASIMPLE } = import.meta.env;
 
-  const urlBancos =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRk6IZqqnmSDcuNEe5ymrviwG259ciagRbszQ-rl1I6wIsDEbwkQs1O67hvQZUNkGIdGcUYskbZyXhi/pub?gid=76171544&single=true&output=csv";
-
-  const urlCuotaSimple =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRk6IZqqnmSDcuNEe5ymrviwG259ciagRbszQ-rl1I6wIsDEbwkQs1O67hvQZUNkGIdGcUYskbZyXhi/pub?gid=773839087&single=true&output=csv";
-
-  const endpoints = [urlPosnet, urlBancos, urlCuotaSimple];
+  const endpoints = [URL_POSNET, URL_BANCOS, URL_CUOTASIMPLE];
 
   const promises = endpoints.map((ep) => fetch(ep).then((res) => res.text()));
 
