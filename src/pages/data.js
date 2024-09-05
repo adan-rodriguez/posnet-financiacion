b5f2dd39ebd2de7988cdb9236e5375c6519e3d7e
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-09-04 21:50:54 -0300

    Fix fetch

--- src/pages/data.js
+++ src/pages/data.js
@@ -6,10 +6,11 @@ export async function GET() {
   const endpoints = [URL_POSNET, URL_BANCOS, URL_CUOTASIMPLE];
 
   const data_csv = await Promise.all(endpoints.map((ep) => fetch(ep))).then(
-    (responses) => {
-      Promise.all(responses.map((response) => response.text()));
-    }
+    (responses) => Promise.all(responses.map((response) => response.text()))
   );
 
-  return new Response(JSON.stringify(data_csv));
+  return new Response(JSON.stringify({ data: data_csv }), {
+    status: 200,
+    headers: { "Content-Type": "application/json;charset=UTF-8" },
+  });
 }
